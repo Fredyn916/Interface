24c6de8bb6009db9f7d37a56891cb76c613815a5
Fred Marques <fredyn.curso@gmail.com>
2024-10-08 17:24:01 -0300

    Atividade 1

---
+++ Exemplo1/Passaro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo1
+{
+    public class Passaro : IAnimal
+    {
+        public void SomAnimal()
+        {
+            Console.WriteLine("Pru pru...");
+        }
+
+        public void Brincar()
+        {
+            Console.WriteLine("Brincando de voar...");
+        }
+
+        public void Comer()
+        {
+            Console.WriteLine("Comendo uma semente...");
+        }
+    }
+}
--- Exemplo1/Program.cs
+++ Exemplo1/Program.cs
@@ -1,6 +1,39 @@
 ﻿using Exemplo1;
+using System.Runtime.CompilerServices;
 
-IAnimal animal = new Gato();
+IAnimal animal;
+
+int resposta = -1;
+
+while (resposta < 0 || resposta > 3)
+{
+    Console.WriteLine("1 - Cachorro");
+    Console.WriteLine("2 - Gato");
+    Console.WriteLine("3 - Pássaro");
+
+    resposta = int.Parse(Console.ReadLine());
+
+    if (resposta < 0 || resposta > 3)
+    {
+        Console.WriteLine("Digite um número de um animal válido");
+    }
+}
+
+switch(resposta)
+{
+    case 1:
+        animal = new Cachorro();
+        break;
+    case 2:
+        animal = new Gato();
+        break;
+    case 3:
+        animal = new Passaro();
+        break;
+    default:
+        animal = new Cachorro();
+        break;
+}
 
 animal.SomAnimal();
 animal.Brincar();
